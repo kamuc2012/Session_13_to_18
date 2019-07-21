920b8c88ff0dede2ff0fc73c979ba5713e3deb67
Kamlesh Chetnani <kamlesh@saleshandy.com>
2019-07-21 23:22:58 +0530

    Added .ipynb files and screenshots

--- Session17/ProblemStatement1.py
+++ Session17/ProblemStatement1.py
@@ -12,48 +12,34 @@ Total   100             98          99          98      395
 
 Question: Are gender and education level dependent at 5% level of significance? In other words, given the data collected above, is there a relationship between the gender of an individual and the level of education that they have obtained?
 """
-import scipy.stats as st
+import pandas as pd
+import scipy.stats as stats
 
-female = [60, 54, 46, 41] #From given table
-male = [40, 44, 53, 57] #From given table
+# Creating data frame with provided dataset
+education = ["High School", "Bachelor", "Masters", "PHD"]
+female = [60, 54, 46, 41]
+male = [40, 44, 53, 57]
+df = pd.DataFrame({"Education": education, "Female": female, "Male": male})
+print(df)
+print("\n", "="*50, "\n", sep="")
 
-female_total = 201 #From given table
-male_total = 194 #From given trom
-people_total = 395 #From given total
-edu_level_total = [100, 98, 99, 98] #From given table
+# Calculate Mean, Standard Deviation, ZScore and P values
+df["Mean_Female"] = df["Female"].mean()
+df["Mean_Male"] = df["Male"].mean()
 
-significance_level = 0.05 #Given, 5% level of significance
+df["StdDev_Female"] = df["Female"].std()
+df["StdDev_Male"] = df["Male"].std()
 
-sample_size = 4 #Given, Education levels: High School, Bachelors, Masters & Ph.d
+df["Z_Female"] = stats.zscore(df["Female"])
+df["Z_Male"] = stats.zscore(df["Male"])
 
-def calc_expected_values(comm_total_lst, seq_total, sample_len): #Calculates expected values 
-    lst = []
-    for val in comm_total_lst:
-        lst.append(val*seq_total/sample_len)
-    return lst
+df["P_Female"] = [stats.norm.cdf(pval) for pval in stats.zscore(df["Female"])]
+df["P_Male"] = [stats.norm.cdf(pval) for pval in stats.zscore(df["Male"])]
 
-expected_female = calc_expected_values(edu_level_total,female_total,people_total) #Calculates expected values for female
+print(df)
+print("\n", "="*50, "\n", sep="")
 
-expected_male = calc_expected_values(edu_level_total,male_total,people_total) #Calculates expected values for male
+print("From the above data analysis - we can conclude that education level depends on the gender.\n\n")
 
-f_obs = female + male 
-print("\nF-Observed (From given table):",f_obs)
-
-f_exp = expected_female + expected_male
-print("\nF-Expected (Calculated from given table):",f_exp)
-
-dof = sample_size - 1 #Degree of freedom of given data set
-
-chi_sq_calc = st.chisquare(f_obs,f_exp) #Calculated Chi-Square for given observed and expected values 
-print("\nChi-Square (Calculated using F-Observed and F-Expected):",chi_sq_calc.statistic)
-
-chi_sq_critical = st.chi2.isf(q=significance_level, df=dof) #Calculates Chi-Square critical values 
-print("\nChi-Square Critical (Calculated using given significance level and degree of freedom):",chi_sq_critical)
-
-#The critical value of Chi-Square with calculated degree of freedom is lesser than the calculated.Therefore we reject the null hypothesis and conclude that the education level depends on gender at a 5% level of significance.
-print("\nIf the critical value of Chi-Square with calculated degree of freedom is lesser than the calculated. Therefore we reject the null hypothesis and conclude that the education level depends on gender at a 5% level of significance.")
-
-if chi_sq_critical < chi_sq_calc.statistic:
-    print("\nReject the null hypothesis and this concludes that the education level depends on gender at a 5% level of significance.")
-else:
-    print("\nNull hypothesis can not be rejected and this concludes that the education level does not depends on gender at a 5% level of significance.")
+print("Male population is higher than Femal population for education level at Masters AND Ph.D.")
+print("Femal population is higher than Male population for education level High-School AND Bachelor")
--- Session17/ProblemStatement2.py
+++ Session17/ProblemStatement2.py
@@ -9,66 +9,61 @@ Write up the results in APA format.
 [Group2: 23, 43, 23, 43, 45]
 [Group3: 56, 76, 74, 87, 56]
 """
-import scipy.stats as st
-import statistics as sts
+import pandas as pd
+import scipy.stats as stats
 
-print("\nLet, Null Hypothesis H0: μ1 = μ2 = μ3 ")
-print("\nLet, Alternate Hypothesis Ha: μ1 ≠ μ2 ≠ μ3 ") 
+df = pd.DataFrame({"Group1": [51, 45, 33, 45, 67], "Group2": [23, 43, 23, 43, 45], "Group3": [56, 76, 74, 87, 56]})
+print(df)
+print("\n", "="*50, "\n", sep="")
 
-print("\nGiven, \n[Group1: 51, 45, 33, 45, 67]")
-print("\n[Group2: 23, 43, 23, 43, 45]")
-print("\n[Group3: 56, 76, 74, 87, 56]")
-    
-group_1 = [51, 45, 33, 45, 67] #Given, [Group1: 51, 45, 33, 45, 67]
-group_2 = [23, 43, 23, 43, 45] #Given, [Group2: 23, 43, 23, 43, 45]
-group_3 = [56, 76, 74, 87, 56] #Given, [Group3: 56, 76, 74, 87, 56]
+'''
+Answer:
+H0:mean_G1 = mean_G2 = mean_G3
+H1:mean_G1 != mean_G2 != mean_G3
 
-n = len(group_1) + len(group_2) + len(group_3) #Count of all the availabale data 
-k = 3 #Given three group of samples
-significance_level = 0.05 #Given, using α=.05
+mean1=(51+ 45+ 33+ 45+ 67)/5
+mean1
+mean2=(23+ 43+ 23+ 43+ 45)/5
+mean2
+mean3=(56+ 76+ 74+ 87+ 56)/5
+mean3
 
-group_1_mean = sts.mean(group_1) #Mean value of Group1
-group_2_mean = sts.mean(group_2) #Mean value of Group2
-group_3_mean = sts.mean(group_3) #Mean value of Group3
-total_mean = sts.mean(group_1 + group_2 + group_3) ##Mean value of all the given groups
+var=(((51-mean1)**2)+((45-mean1)**2)+((33-mean1)**2)+((45-mean1)**2)+((67-mean1)**2))/4
+var2=(((23-mean2)**2)+((43-mean2)**2)+((23-mean2)**2)+((43-mean2)**2)+((45-mean2)**2))/4
+var3=(((56-mean3)**2)+((76-mean3)**2)+((74-mean3)**2)+((87-mean3)**2)+((56-mean3)**2))/4
 
-dof_between = k-1 #Degree of freedom between
-print("\nDegree of freedom between (dof_between):",dof_between)
+print(var,var2,var3)
 
-dof_within= n-k #Degree of freedom within
-print("\nDegree of freedom within (dof_within):",dof_within)
+MS_error=(153.2+128.8+183.2)/3=155.07
+df_error=15−3=12
+SS_error=(155.07)(15−3)=1860.8
 
-group_size = len(group_1) #From given group
+Grand mean,  x_bar_grand=(48.2+35.4+69.8)/3=51.13
 
-#Sum of Squares Between
-ss_between = (((group_1_mean-total_mean)**2) + ((group_2_mean-total_mean)**2) + ((group_3_mean-total_mean)**2)) * group_size
-print("\nSum of Squares Between (ss_between):",ss_between)
+ group mean grand mean deviations sq deviations
+       48.2      51.13      -2.93          8.58
+       35.4      51.13     -15.73        247.43
+       69.8      51.13      18.67        348.57
+       
+Var_means=(604.58)/3−1=302.29
 
-ms_between = ss_between/dof_between #Mean Square Between
-print("\nMean Square Between (ms_between = ss_between/dof_between):",ms_between)
+MS_between=(302.29)*(5)=1511.45
 
-def calc_sum_of_square(sequence, mean): #Calculates sum of squares for given sequence and mean.
-    sum_val = 0
-    for val in sequence:
-        sum_val += ((val-mean)**2)
-    return sum_val
+Test statistic and critical value
 
-#Sum of Squares Within
-ss_within = calc_sum_of_square(group_1, group_1_mean) + calc_sum_of_square(group_2, group_2_mean) + calc_sum_of_square(group_3, group_3_mean)
-print("\nSum of Squares Within (ss_within):",ss_within)
+    F=1511.45/155.07=9.75
+    Fcritical(2,12)=3.89 (using F table)
 
-ms_within = ss_within/dof_within #Mean Square Within
-print("\nMean Square Within (ms_within = ss_within/dof_within):",ms_within)
+since 9.75>3.89, Hence we reject H0
 
-F_Calc = ms_between/ms_within #F-Satistic
-print("\nF-Calculated (ms_between/ms_within):",F_Calc)
+ANOVA table
+source  SS      df      MS     F
+group   1057.5   2    528.75 3.93
+error  11703.82 87    134.53
+total  12761.32
 
-#Calculating F-Critical using given significance level and degree of freedom
-F_Critical = st.f.ppf(q=1-significance_level, dfn=dof_between, dfd=dof_within) 
-print("\nF-Critical:",F_Critical)
-
-print("\nIf the critical value of F from tables is less than the calculated value of F, reject the null hypothesis")
-if F_Critical < F_Calc:
-    print("\nReject Null Hypothesis H0 and conclude that at least two of the means are significantly different from each other.")
-else:
-    print("\nNull Hypothesis H0 can not be rejected and conclude that all the means are equal.")
+Effect size
+    nita_2=1057.5/12761.32=0.08
+APA write up 
+    F(2, 12)=9.75, p <0.05, nita_2=0.62.
+'''
